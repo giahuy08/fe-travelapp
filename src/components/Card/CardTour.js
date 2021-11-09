be7610fabe6af0a9291431d62bd73b38322c1498
chanh2000kh <55187339+chanh2000kh@users.noreply.github.com>
2021-11-09 07:36:37 +0700

    CardTour

---
+++ src/components/Card/CardTour.js
@@ -0,0 +1,25 @@
+import React from "react";
+function CardTour(props) {
+  return (
+    <>
+    <div className='cards__item_tour'>
+      <article>
+      <figure class='cards__item__pic-wrap' data-category={props.price} >
+                    <a href="/#">                   
+                        <img className='cards__item__img' src={props.img}/>
+                        <div class='tour_day'>
+                    <p class="">{props.days}</p>
+                </div>
+                    </a>
+                </figure>
+                <div className="cards__item__info">
+              <h5 className="cards__item__text">{props.text}</h5>
+              </div>                                                         
+                  
+        </article>                          
+            </div>        
+    </>
+  );
+}
+
+export default CardTour;
