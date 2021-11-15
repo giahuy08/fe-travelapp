6f7bd77780d540cd49329e8ad034be59085efd11
thienviva <thienviva7761311@gmail.com>
2021-11-15 15:19:24 +0700

    [ThienHN]

---
+++ src/api/apiService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+let API_URL="http://localhost:5000";
+
+export default function callApi(endpoint, method='GET',body){
+    return axios({
+        method,
+        url:`${API_URL}/${endpoint}`,
+        headers:{
+            "Authorization":"Bearer " + localStorage.getItem("accessToken")
+        },
+        data:body,
+    })
+    // .catch(e=>{
+    //     console.log(e)
+    // })
+}
+
+
