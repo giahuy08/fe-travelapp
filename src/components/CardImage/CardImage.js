2cdf3580103cc79456c027f95262868358c0f609
thienviva <thienviva7761311@gmail.com>
2021-11-16 17:02:59 +0700

    [ThienHN]

--- src/components/CardImage/CardImage.js
+++ src/components/CardImage/CardImage.js
@@ -1,18 +1,18 @@
 import React from "react";
 import './CardImage.css'
-function CardImage() {
+function CardImage(props) {
   return (
     <div>
       <a href="" class="home__pro__place-link">
         <img
-          src="/images/location_5_1559735011.png"
-          alt="Hà Nội"
+          src={props.image}
+          alt={props.name}
           class="home__pro__place-img"
         />
         <div class="home__pro__place-title">
-          <span class="home__pro__place-label">Hà Nội </span>
-          <span class="home__pro__place-decs">2822 </span>
-          <span class="home__pro__place-decs-place">Chỗ ở</span>
+          <span class="home__pro__place-label">{props.name} </span>
+          <span class="home__pro__place-decs">{props.numbers} </span>
+          <span class="home__pro__place-decs-place">Địa điểm</span>
         </div>
       </a>
     </div>
