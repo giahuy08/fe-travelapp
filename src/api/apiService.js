b3f31397a137b50f791b2fff0a751793106935ba
18110121 <18110121@student.hcmute.edu.vn>
2022-03-27 16:56:08 +0700

    [Huy] upcode

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let API_URL="https://app-travelbe.herokuapp.com";
+let API_URL="https://be-travel.herokuapp.com";
 
 export default function callApi(endpoint, method='GET',body){
     return axios({
